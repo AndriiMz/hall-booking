418d151968a88c576e99b30f3dc9e3d18c9eb759
andrii <moroz97andre@mail.ua>
2019-01-20 17:34:00 +0100

    fix bugs

--- src/Service/RegistrationService.php
+++ src/Service/RegistrationService.php
@@ -51,12 +51,41 @@ class RegistrationService
         }
 
         $user = new Client();
-        $user->setFirstName($request->get('firstName'));
-        $user->setEmail($request->get('email'));
-        $user->setUsername($request->get('username'));
-        $user->setPhone($request->get('phone'));
+
+        $firstName = $request->get('firstName');
+
+        if (empty($firstName)) {
+            throw new \Exception('Pole "Imie" nie może być puste');
+        }
+        $user->setFirstName($firstName);
+
+        $email = $request->get('email');
+        if (empty($email)) {
+            throw new \Exception('Pole "E-mail" nie może być puste');
+        }
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \Exception('Niepoprawny "E-mail"');
+        }
+
+        $user->setEmail($email);
+
+
+        if (empty($username)) {
+            throw new \Exception('Pole "Login" nie może być puste');
+        }
+        $user->setUsername($username);
+
+        $phone = $request->get('phone');
+        $user->setPhone($phone);
+        if (empty($phone)) {
+            throw new \Exception('Pole "Telefon" nie może być puste');
+        }
 
         $password = $request->get('password');
+
+        if (empty($password)) {
+            throw new \Exception('Pole "Hasło" nie może być puste');
+        }
         $user->setPlainPassword($password);
         $user->setPassword($this->encoder->encodePassword($user, $password));
 
